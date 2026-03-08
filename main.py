5c00e59cab31c1cb24b3a90e7c871ea78b84f66f
d0duhsk <m.dashdondov1@gmail.com>
2026-03-08 21:59:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import discord
+from discord.ext import commands
+import os
 
+TOKEN = os.getenv("TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="S ", intents=intents)
+
+@bot.event
+async def on_ready():
+    print("Bot online!")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+bot.run(TOKEN)
